0fc7fb1287da17f32d34ba83abc80b3aa792552f
jinhyun <free1330@gmail.com>
2016-06-27 21:12:58 +0900

    feature: 로그아웃

--- src/main/java/com/cocotask/core/login/rest/LoginRestController.java
+++ src/main/java/com/cocotask/core/login/rest/LoginRestController.java
@@ -3,10 +3,7 @@ package com.cocotask.core.login.rest;
 import com.cocotask.core.login.service.LoginService;
 import com.cocotask.core.user.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -19,5 +16,10 @@ public class LoginRestController {
     @PostMapping("/login")
     public User doLogin(@RequestBody User userInput, HttpSession session) throws Exception {
         return loginService.doLogin(userInput, session);
+    }
+
+    @GetMapping("/logout")
+    public void doLogout(HttpSession session) throws Exception {
+        loginService.doLogout(session);
     }
 }
--- src/main/java/com/cocotask/core/login/service/LoginService.java
+++ src/main/java/com/cocotask/core/login/service/LoginService.java
@@ -43,4 +43,8 @@ public class LoginService {
 
         return user;
     }
+
+    public void doLogout(HttpSession session) {
+        session.invalidate();
+    }
 }
